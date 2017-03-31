9c068eabb0f37611f3511d2617cc36834c6c6572
Scott Cowan <info@scottcowan.org>
2017-03-31 16:57:27 +0100

    adding nuspecs and appveyor config

--- src/Ukrlp.SoapApi.Types/Provider.cs
+++ src/Ukrlp.SoapApi.Types/Provider.cs
@@ -4,6 +4,9 @@ namespace Ukrlp.SoapApi.Types
 {
     public class Provider
     {
+        /// <summary>
+        /// Provider ID : 8 characters long
+        /// </summary>
         public string UnitedKingdomProviderReferenceNumber { get; set; }
         public string ProviderName { get; set; }
         public IEnumerable<Contact> ProviderContact { get; set; }
